be7b5cc7879231d55a65b7d3843d0ca50700ddc8
miyemicodes <lizzymiyemi@gmail.com>
2025-02-21 14:22:58 +0100

    Update project

--- src/assets.js
+++ src/assets.js
@@ -3,26 +3,37 @@ import asset003 from "./assets/checkout.png";
 import asset004 from "./assets/LDH.png";
 import asset005 from "./assets/01.png";
 import asset006 from './assets/Ewebsite.png';
+import asset007 from './assets/Dashboard 1.png';
+
 
 const assets = [
   {
     id: 1,
+    projectname: "E-Commerce",
+    description:
+      "This e-commerce website, built with React.js and styled using Tailwind CSS, offers a seamless shopping experience with a modern, responsive design optimized for various devices. It includes key features such as dynamic product browsing with filtering and search capabilities, a fully functional shopping cart, and an intuitive checkout process. The application is designed with a modular architecture, relying heavily on reusable components like buttons, product cards, and modals, which enhance maintainability and scalability. Leveraging React Hooks, the website efficiently manages state and side effects, ensuring smooth user interactions. Additionally, React Router facilitates fast, client-side navigation, providing a seamless browsing experience without full-page reloads. The implementation adheres to modern development practices, focusing on performance optimizations and ensuring a lightweight and efficient application. This makes the platform not only user-friendly but also developer-centric, capable of evolving with new features and requirements.",
+    img: asset006,
+    websiteLinks: "https://e-commerce-website-psi-three.vercel.app/",
+  },
+  {
+    id: 2,
+    projectname: "Admin Dashboard",
+    description:
+      "A sleek and responsive admin dashboard built with Next.js, TypeScript, Tailwind CSS, and Chart.js. It features dynamic data visualization, intuitive navigation, and interactive UI components for seamless management and analytics. Optimized for performance and scalability, it provides a user-friendly experience for tracking key metrics and managing operations efficiently.",
+    img: asset007,
+    websiteLinks: "https://dashboard-test-lovat.vercel.app/",
+  },
+  {
+    id: 3,
     projectname: "Tourest",
     description:
       "This page offers a visual feast of breathtaking landscapes, cultural experiences, and exciting adventures waiting to be discovered. Immerse yourself in the vibrant imagery and concise, informative sections. With an intuitive and responsive layout, this landing page ensures a delightful experience across devices. Join us on a digital voyage that sparks your curiosity and fuels your passion for exploration.",
     img: asset001,
     websiteLinks: "https://tourest-five.vercel.app/",
   },
+
   {
-    id: 2,
-    projectname: "Checkout",
-    description:
-      "Our streamlined and user-friendly checkout page, designed to enhance the overall shopping experience for our customers. This intuitive and efficient platform ensures purchase completion. With a clean and responsive layout, the checkout process becomes effortlessly navigable across various devices. Customers can review their items, input shipping details, and select preferred payment methods with ease. Simplify the path to purchase with our carefully crafted checkout page.",
-    img: asset003,
-    websiteLinks: "https://my-checkout-kappa.vercel.app/",
-  },
-  {
-    id: 3,
+    id: 4,
     projectname: "LDH",
     description:
       "LDH, a sleek and modern showcase of our brand. Crafted with user-centric design in mind, this landing page seamlessly adapts to various devices, providing an optimal viewing experience for potential clients and partners. The page features a clean layout, engaging visuals, and concise information about our company's mission, values, and offerings. With a focus on simplicity and accessibility, visitors can effortlessly navigate through key sections to learn more about our services and how we stand out in the industry. Elevate your first impression with our responsive company landing page — a gateway to discovering the essence of LDH.",
@@ -30,20 +41,20 @@ const assets = [
     websiteLinks: "https://ldh-portfolio.vercel.app/",
   },
   {
-    id: 4,
-    projectname: "Dashboard",
+    id: 5,
+    projectname: "Shopify Dashboard",
     description:
       "This user-friendly interface provides a centralized space for quick insights into key e-commerce metrics, including sales performance, order status, and inventory levels. Its responsive layout ensures accessibility across devices, offering a seamless experience for merchants seeking a comprehensive overview of their Shopify store's performance.",
     img: asset005,
     websiteLinks: "https://my-dashboard-nine-tau.vercel.app/",
   },
   {
-    id: 5,
-    projectname: "E-Commerce",
+    id: 6,
+    projectname: "Checkout",
     description:
-      "This e-commerce website, built with React.js and styled using Tailwind CSS, offers a seamless shopping experience with a modern, responsive design optimized for various devices. It includes key features such as dynamic product browsing with filtering and search capabilities, a fully functional shopping cart, and an intuitive checkout process. The application is designed with a modular architecture, relying heavily on reusable components like buttons, product cards, and modals, which enhance maintainability and scalability. Leveraging React Hooks, the website efficiently manages state and side effects, ensuring smooth user interactions. Additionally, React Router facilitates fast, client-side navigation, providing a seamless browsing experience without full-page reloads. The implementation adheres to modern development practices, focusing on performance optimizations and ensuring a lightweight and efficient application. This makes the platform not only user-friendly but also developer-centric, capable of evolving with new features and requirements.",
-    img: asset006,
-    websiteLinks: "https://e-commerce-website-psi-three.vercel.app/",
+      "Our streamlined and user-friendly checkout page, designed to enhance the overall shopping experience for our customers. This intuitive and efficient platform ensures purchase completion. With a clean and responsive layout, the checkout process becomes effortlessly navigable across various devices. Customers can review their items, input shipping details, and select preferred payment methods with ease. Simplify the path to purchase with our carefully crafted checkout page.",
+    img: asset003,
+    websiteLinks: "https://my-checkout-kappa.vercel.app/",
   },
 ];
 
